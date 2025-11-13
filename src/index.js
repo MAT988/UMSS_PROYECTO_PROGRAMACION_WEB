fd605b136a15fc5d113b8d95c9e25267289e2ea3
Leonel Zeballos Aldunate <zeballos.leonel@outlook.com>
2025-11-13 18:40:06 -0400

    init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import Server from './config/server.config.js';
+import { PORT } from './config/env.config.js';
+
+async function startServer() {
+  try {
+    Server.listen(PORT, () => {
+      console.info(`Server running on http://localhost:${PORT}`);
+    });
+  } catch(error) {
+    console.log("Error starting server", error);
+  }
+}
+
+startServer();
