b7d5a46adda2c7500b6e96d9ed3f741b1e7e79ec
MAT988 <mateomerino988@gmail.com>
2025-11-25 22:28:59 -0400

    Avance Final de Endpoints

--- src/models/index.js
+++ src/models/index.js
@@ -1,2 +1,4 @@
 export { default as Categoria } from './categoria.model.js';
 export { default as Subcategoria } from './subcategoria.model.js';
+export { default as NivelDificultad } from './nivel_dificultad.model.js';
+export { default as RangoEdad } from './rango_edad.model.js';
