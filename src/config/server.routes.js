8ab9398040f2c9dcebc0fab7dce409e00637c0b5
Leonel Zeballos Aldunate <zeballos.leonel@outlook.com>
2025-12-09 20:27:33 -0400

    add: middleware for restrict roles

--- src/config/server.routes.js
+++ src/config/server.routes.js
@@ -6,26 +6,28 @@ import CategoriaRoutes from '../modules/categoria/categoria.routes.js';
 import SubcategoriaRoutes from '../modules/subcategoria/subcategoria.routes.js';
 import RolRoutes from '../modules/rol/rol.routes.js';
 import AuthRoutes from '../modules/auth/auth.routes.js';
-import { verifyToken } from '../middlewares/auth.middleware.js';
+import { verifyToken, restrictTo } from '../middlewares/auth.middleware.js';
 
-const router=Router();
+const router = Router();
 
-router.use('/api/auth',AuthRoutes);
-router.use('/api',HealthRoutes);
-router.use('/api/categorias',verifyToken,CategoriaRoutes);
-router.use('/api/subcategorias',verifyToken,SubcategoriaRoutes);
-router.use('/api/rangos-edad',verifyToken,RangoEdadRoutes);
-router.use('/api/niveles-dificultad',verifyToken,NivelDificultadRoutes);
-router.use('/api/rol',RolRoutes);
+router.use('/api/auth', AuthRoutes);
+router.use('/api', HealthRoutes);
+router.use('/api/niveles-dificultad', verifyToken, NivelDificultadRoutes);
 
-router.use('/',(req, res)=>{
+router.use('/api/rol', verifyToken, restrictTo('administrador'), RolRoutes);
+
+router.use('/api/categorias', verifyToken, restrictTo('administrador', 'profesor'), CategoriaRoutes);
+router.use('/api/subcategorias', verifyToken, restrictTo('administrador', 'profesor'), SubcategoriaRoutes);
+router.use('/api/rangos-edad', RangoEdadRoutes);
+
+router.use('/', (req, res) => {
   res.json({ message: 'Ejecutando servidor HTTP :v'});
 });
 
-router.use((req, res)=>{
-  console.log('Not found:',req.method,req.originalUrl);
+router.use((req, res) => {
+  console.log('Not found:', req.method, req.originalUrl);
   res.status(404).send({
-    message:'route not found',
+    message: 'route not found',
   });
 });
 
